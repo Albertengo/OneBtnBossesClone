e4de21b0bbf6f7ac2275409b032343661d294565
Albertengo <145947798+Albertengo@users.noreply.github.com>
2024-11-04 23:09:59 -0300

    sacando archivos que no sirven

--- Assets/Scripts/Player/MovimientoPlayer.cs
+++ Assets/Scripts/Player/MovimientoPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movimiento : MonoBehaviour
+public class MovimientoPlayer : MonoBehaviour
 {
     float rotation = 0;
     public float speed;
